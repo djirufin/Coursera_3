686fdc60cce2945824a4bd1c28f8d354b68d7453
djirufin <rufin.dji@gmail.com>
2020-02-10 17:53:46 +0000

    Final version of run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -22,17 +22,25 @@ dftest <- act_y_test %>% rename("Activity" = V2) %>% select(SubjectID, Activity,
 
 
 #Dealing with train files
-x_train <- read.table(paste(folder_list[[1]], '/X_', folder_list[[1]], '.txt', sep = ''))
-y_train <- read.table(paste(folder_list[[1]], '/y_', folder_list[[1]], '.txt', sep = ''))
-sub_train <- read.table(paste(folder_list[[1]], '/subject_', folder_list[[1]], '.txt', sep = ''))
+x_train <- read.table(paste(folder_list[[2]], '/X_', folder_list[[2]], '.txt', sep = ''))
+y_train <- read.table(paste(folder_list[[2]], '/y_', folder_list[[2]], '.txt', sep = ''))
+sub_train <- read.table(paste(folder_list[[2]], '/subject_', folder_list[[2]], '.txt', sep = ''))
 
 colnames(x_train) <- features$V2
 colnames(sub_train) <- "SubjectID"
 x_train <- cbind(y_train, sub_train, x_train)
-act_y_train <- merge(x_train, activity_label, by="V1", all = TRUE, sort = FALSE)
+act_y_train <- merge(x_train, activity_label, by="V2", all = TRUE, sort = FALSE)
 act_y_train <- act_y_train[, !duplicated(colnames(act_y_train))]
-dftrain <- act_y_train %>% rename("Activity" = V2) %>% select(SubjectID, Activity, everything()) %>%select(-V1)
+dftrain <- act_y_train %>% rename("Activity" = V2) %>% select(SubjectID, Activity, everything()) %>%select(-V2)
 
 
 dffinal <- rbind(dftest, dftrain)
+
+df_mean_std <- dffinal %>% select(matches("(SubjectID|Activity|.mean.|.std.)", ignore.case = FALSE))
+
+library(stringr)
+df_mean_std <- df_mean_std %>% rename_all(funs(str_remove_all(., "\\(\\)")))
+
+cdf <- df_mean_std %>% group_by(Activity, SubjectID) %>% summarise_all(funs(mean))
+
 
